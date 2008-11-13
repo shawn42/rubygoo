9f3ca027704fb68760088628a38823b41019ae8f
shawn42 <shawn42@ba578ba7-3053-0410-b551-57805770c6e6>
2008-11-13 21:00:33 +0000

    basic workings of a checkgroup, need to pull this into its own widget
    git-svn-id: http://rubygoo.googlecode.com/svn/trunk@50 ba578ba7-3053-0410-b551-57805770c6e6

--- samples/create_gui.rb
+++ samples/create_gui.rb
@@ -41,11 +41,38 @@ module CreateGui
       modal.show
     end
 
+    panel = Container.new :x=>10, :y=>380, :x_pad=>20, :y_pad=>20, :w=> 500, :h=>80
+    panel_label = Label.new "On a scale of one to three", :x=>40, :y=>10, :w=>20, :h=>20, :relative=>true
+    panel_check_one = CheckBox.new :x=>40, :y=>40, :w=>20, :h=>20, :relative=>true
+    panel_check_two = CheckBox.new :x=>90, :y=>40, :w=>20, :h=>20, :relative=>true
+    panel_check_three = CheckBox.new :x=>140, :y=>40, :w=>20, :h=>20, :relative=>true
+
+    panel_check_one.when :checked do
+      if panel_check_one.checked?
+        panel_check_two.uncheck
+        panel_check_three.uncheck
+      end
+    end
+    panel_check_two.when :checked do
+      if panel_check_two.checked?
+        panel_check_one.uncheck
+        panel_check_three.uncheck
+      end
+    end
+    panel_check_three.when :checked do
+      if panel_check_three.checked?
+        panel_check_two.uncheck
+        panel_check_one.uncheck
+      end
+    end
+
+    panel.add panel_label, panel_check_one, panel_check_two, panel_check_three
+
     # implicit tab ordering based on order of addition, can
     # specify if you want on widget creation
 
     # can add many or one at a time
-    app.add text_field, label, button, modal_button
+    app.add text_field, label, button, modal_button, panel
     app.add check
 
   #  pulldown = Pulldown.new {:x=>70, :y=>80}
