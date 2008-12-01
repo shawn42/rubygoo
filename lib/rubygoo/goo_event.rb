a325872ea1ce42fafda41509785713350fef5fc2
shawn42 <shawn42@ba578ba7-3053-0410-b551-57805770c6e6>
2008-12-01 19:51:33 +0000

    added handled placeholder on event for event chain management
    git-svn-id: http://rubygoo.googlecode.com/svn/trunk@65 ba578ba7-3053-0410-b551-57805770c6e6

--- lib/rubygoo/goo_event.rb
+++ lib/rubygoo/goo_event.rb
@@ -2,7 +2,7 @@ module Rubygoo
   # all events in the system are converted to these for internal use by our
   # adapter
   class GooEvent
-    attr_accessor :event_type, :data
+    attr_accessor :event_type, :data, :handled
 
     def initialize(event_type, event_data = nil)
       @event_type = event_type
