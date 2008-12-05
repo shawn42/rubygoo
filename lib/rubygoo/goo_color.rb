d0fabe23332b89a5be4635666d098fa4d695c0ff
shawn42 <shawn42@ba578ba7-3053-0410-b551-57805770c6e6>
2008-12-05 23:00:48 +0000

    added static method for creating GooColor objects
    git-svn-id: http://rubygoo.googlecode.com/svn/trunk@77 ba578ba7-3053-0410-b551-57805770c6e6

--- lib/rubygoo/goo_color.rb
+++ lib/rubygoo/goo_color.rb
@@ -8,5 +8,12 @@ module Rubygoo
     def initialize(r,g,b,a)
     @r,@g,@b,@a = r,g,b,a 
     end
+
+    def self.color(color, alpha=nil)
+      c = COLORS[color]
+      c ||= [0,0,0,255]
+      c[3] = alpha if alpha
+      GooColor.new *c
+    end
   end
 end
